3adf82a3b9b8874c487ef8068fafbbfe15e4c076
zairl23 <756127792@qq.com>
2016-03-18 10:48:24 +0800

    add

---
+++ src/public/index.php
@@ -0,0 +1,19 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->get('/', function (Request $request, Response $response){
+  $sub = str_replace(".domain.com", "", $_SERVER["HTTP_HOST"]);
+  echo $sub;
+});
+
+$app->run();
